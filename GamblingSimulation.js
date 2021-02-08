ec6770c00b72ce92ac9a7712f482c60428148471
Aanchalt14 <aanchalt71@gmail.com>
2021-02-09 00:30:28 +0530

    Add : Making bet either win or lose

--- GamblingSimulation.js
+++ GamblingSimulation.js
@@ -1,3 +1,13 @@
 console.log('Welcome to Gambling Simulation')
 var stakeAmount=100;
 var game=1
+var result =Math.floor(Math.random*2);
+var win=1;
+var lose=2;
+if(result==win)
+{
+    stakeAmount++;
+}
+else{
+    stakeAmount--;
+}
