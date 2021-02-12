86ce7a05b16988531c55780bfeddbb3548a53be1
Aanchalt14 <aanchalt71@gmail.com>
2021-02-12 23:31:40 +0530

    Add : Total Amount won by gambler

--- GamblingSimulation.js
+++ GamblingSimulation.js
@@ -1,3 +1,23 @@
 console.log('Welcome to Gambling Simulation')
 var stakeAmount=100;
+var Amount=50;     //amount assign when win or lose
 var game=1
+var win=1;
+var lose=2;
+var totalAmount=0;
+for(let i=1;i<=20;i++){
+  var result =Math.floor(Math.random()*2)
+  console.log(result)
+  if(result==1) 
+   {
+     stakeAmount=+Amount;
+     console.log('WIN :' ,stakeAmount );
+    }
+ else{
+      stakeAmount-=Amount;
+      console.log('LOSE:' ,stakeAmount );
+    
+    }
+    totalAmount+=stakeAmount;
+    console.log('Total Amount is :',totalAmount)
+}
