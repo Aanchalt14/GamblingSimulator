37eef81455e4d53ff8713c8ea5f2d6a2ce811827
Aanchalt14 <aanchalt71@gmail.com>
2021-02-12 23:19:46 +0530

    Add : If gambler won the bet 50% gain otherwise lost 50%

--- GamblingSimulation.js
+++ GamblingSimulation.js
@@ -1,3 +1,16 @@
 console.log('Welcome to Gambling Simulation')
 var stakeAmount=100;
+var Amount=50;     //amount assign when win or lose
 var game=1
+var win=1;
+var lose=2;
+  var result =Math.floor(Math.random()*2)
+  if(result==1) 
+   {
+     stakeAmount=+Amount;
+     console.log('WIN :' ,stakeAmount);
+    }
+ else{
+      stakeAmount-=Amount;
+      console.log('LOSE:' ,stakeAmount );
+    }
